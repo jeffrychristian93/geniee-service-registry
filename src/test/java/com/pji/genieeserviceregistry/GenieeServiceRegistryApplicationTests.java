c5b25f1905b3406bd181c1de623979336500ded2
Jeffry <jeffry.christian@pji-solutions.com>
2018-05-15 08:49:09 +0700

    f commit

---
+++ src/test/java/com/pji/genieeserviceregistry/GenieeServiceRegistryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pji.genieeserviceregistry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GenieeServiceRegistryApplicationTests {
+
+    @Test
+    public void contextLoads(){
+    }
+
+}
